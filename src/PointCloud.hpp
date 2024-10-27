f843ed6b43c04ffbb8f295fecd6a3f6d95d78cbc
tom380 <twjfransen@gmail.com>
2024-10-27 16:19:55 +0100

    Cleanup main

--- src/PointCloud.hpp
+++ src/PointCloud.hpp
@@ -3,6 +3,7 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 #include <vector>
+#include <string>
 
 struct Point {
     float x;
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
-
-#include <iostream>
-#include <string>
-#include <math.h>
-
 #include "Stopwatch.hpp"
 #include "Image.hpp"
 #include "PointCloud.hpp"
+
+#include <algorithm>
+#include <cmath>
 
 #define PI 3.14159265359
 #define TAU 6.28318530718
 
 #define NAILS 100
 #define THREAD_WIDTH 1 // Pixels
-
-auto start = std::chrono::high_resolution_clock::now();
 
 int main() {
     Stopwatch timer;
@@ -43,50 +39,6 @@ int main() {
     pointCloud.render("cutoutChopper.png");
     timer.lap("Saving cutout image");
 
-
-    // double* intensity = new double[img.width() * img.height()];
-
-    // for (int x = 0; x < img.width(); x++) {
-    //     for (int y = 0; y < img.height(); y++) {
-    //         intensity[y*img.width() + x] = img.intensity(x, y);
-    //     }
-    // }
-    // timer.lap("Intensity calculation");
-
-    // unsigned char* grayscale = new unsigned char[img.width() * img.height()];
-
-    // for (int x = 0; x < img.width(); x++) {
-    //     for (int y = 0; y < img.height(); y++) {
-    //         grayscale[y*img.width() + x] = (unsigned char)(255 * intensity[y*img.width() + x]);
-    //     }
-    // }
-    // timer.lap("Convert to greyscale");
-
-    // stbi_write_png("grayChopper.png", img.width(), img.height(), 1, grayscale, img.width());
-    // // delete[] grayscale;
-    // timer.lap("Saving grayscale image");
-
-    // int centre_x = img.width() / 2;
-    // int centre_y = img.height() / 2;
-    // int radius = std::min(centre_x, centre_y);
-    // int radius_sqr = radius * radius;
-
-    // for (int x = 0; x < img.width(); x++) {
-    //     for (int y = 0; y < img.height(); y++) {
-    //         if ((x - centre_x)*(x - centre_x) + (y - centre_y)*(y - centre_y) <= radius_sqr) {
-
-    //         }
-    //         else grayscale[y*img.width() + x] = 0;
-    //     }
-    // }
-    // timer.lap("Create cutout");
-
-    // stbi_write_png("cutoutChopper.png", img.width(), img.height(), 1, grayscale, img.width());
-    // delete[] grayscale;
-    // timer.lap("Saving cutout image");
-
-    
-
     // for (int i = 0; i < NAILS / 2; i++) {
     //     const float angle = (float)i / NAILS * TAU;
     //     const float cos = std::cos(angle);
@@ -103,9 +55,6 @@ int main() {
     //     }
     // }
     // timer.lap("Loop");
-
-    
-
 
     timer.stop("Total");
     return 0;
