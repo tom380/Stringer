3d9d811bd3edfd60d969fb19062d6fc215a68b3a
tom380 <twjfransen@gmail.com>
2024-10-28 19:36:21 +0100

    Added radon conversion (not working correctly)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "Stopwatch.hpp"
 #include "Image.hpp"
 #include "PointCloud.hpp"
+// #define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
 
 #include <algorithm>
 #include <cmath>
@@ -10,6 +12,15 @@
 
 #define NAILS 100
 #define THREAD_WIDTH 1 // Pixels
+
+struct Bin {
+    float pos;
+    float width;
+
+    bool isIn(const float &point) {
+        return (point > pos - width / 2) && (pos + width / 2 < point);
+    }
+};
 
 int main() {
     Stopwatch timer;
@@ -39,16 +50,49 @@ int main() {
     pointCloud.render("cutoutChopper.png");
     timer.lap("Saving cutout image");
 
-    for (int i = 0; i < NAILS / 2; i++) {
-        const float angle = (float)i / NAILS * TAU;
+    std::vector<Bin> lines_on;
+    for (int nail = 1; nail < NAILS / 2; nail++) {
+        const float angle = (float)nail / NAILS * TAU;
+        lines_on.push_back(Bin{std::cos(angle),1});
+    }
+    std::vector<Bin> lines_between;
+    for (int nail = 0; nail < NAILS / 2; nail++) {
+        const float angle = (nail + 0.5) / NAILS * TAU;
+        lines_between.push_back(Bin{std::cos(angle),1});
+    }
+    timer.lap("Calculate bins");
+
+    float line_intensities[NAILS][NAILS] = {0};
+    float max_intensity = 0;
+    for (int nail = 0; nail < NAILS / 2; nail++) {
+        const float angle = (float)nail / NAILS * TAU;
         const float cos = std::cos(angle);
         const float sin = std::sin(angle);
 
         for (Point point : pointCloud) {
             const float projection = point.x * cos + point.y * sin;
+        
+            for (int line = 0; line < lines_on.size(); line++) {
+                if (lines_on[line].isIn(projection)) {
+                    line_intensities[nail + line + 1][(NAILS + nail - line - 1) % NAILS] += point.data;
+                    if (line_intensities[nail + line + 1][(NAILS + nail - line - 1) % NAILS] > max_intensity) {
+                        max_intensity = line_intensities[nail + line + 1][(NAILS + nail - line - 1) % NAILS];
+                    }
+                }
+            }
         }
     }
     timer.lap("Loop");
+
+    unsigned char* image = new unsigned char[NAILS * NAILS]();
+    for (int n1 = 0; n1 < NAILS; n1++) {
+        for (int n2 = 0; n2 < NAILS; n2++) {
+            image[n1 + NAILS * n2] = (unsigned char)(line_intensities[n1][n2] / max_intensity * 255);
+        }
+    }
+    stbi_write_png("transform.png", NAILS, NAILS, 1, image, NAILS);
+    delete[] image;
+    timer.lap("Save transform");
 
     timer.stop("Total");
     return 0;
